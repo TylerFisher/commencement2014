aa60bf8f3d1918914453bacad986f3b2d46e17bb
Denise Lu <denise.ds.lu@gmail.com>
2014-06-15 16:39:02 -0400

    three ideas for list-block designs

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
 		$(this).parent().find('.mask').css('opacity','0.6');
 	});
 
+	// $('.idea3 .mask-text').on('mouseover',function(){
+	// 	$(this).parent().find('.mask').show();
+	// }).on('mouseout',function(){
+	// 	$(this).parent().find('.mask').hide();
+	// });
+
 
 });
 

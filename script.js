3db1f5f1a8c1d0b37a8bb7ef9aa5487be2be3d7f
Denise Lu <denise.ds.lu@gmail.com>
2014-06-15 14:20:54 -0400

    making responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.mask-text').on('mouseover',function(){
+		$(this).parent().find('.mask').css('opacity','0.4');
+	}).on('mouseout',function(){
+		$(this).parent().find('.mask').css('opacity','0.6');
+	});
+});
+
+
+
+
